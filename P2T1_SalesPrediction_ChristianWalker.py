9c942835784f276ac20353e0d5c1473c8232cb67
Christian Walker <cwal7887@gmail.com>
2019-09-12 12:20:33 -0400

    9/12/2019

---
+++ P2T1_SalesPrediction_ChristianWalker.py
@@ -0,0 +1,17 @@
+#Christian Walker
+#9/12/2019
+#Sales Prediction P2T1
+#Calculate profit of total sales
+
+#input projected sales
+totalsales = float(input("Enter the projected sales: $"))
+
+#calculate the profit as 23% of total sales
+profit = totalsales * 0.23
+
+#display the profit
+print("The profit is $", profit)
+
+
+
+
