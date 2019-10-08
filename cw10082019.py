7a649109bd9eb21f30919ddf3b2687650539a7c9
Christian Walker <cwal7887@gmail.com>
2019-10-08 12:02:40 -0400

    While Loops Intro
    10/08/2019

---
+++ cw10082019.py
@@ -0,0 +1,12 @@
+#Christian Wlaker
+#10/08/2019
+#cw10082019
+#While loops
+
+var = "yes"
+while var.lower() == "yes":
+    print("yeah yeah yeah")
+    var = input("Do you want to run the program again? Enter yes or no: ")
+input("Press any key to exit")
+
+
